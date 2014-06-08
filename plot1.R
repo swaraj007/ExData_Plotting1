01987d4a56d71a939f29f3e4747a74703eba99c2
swaraj007 <swarajkumar.224@gmail.com>
2014-06-09 02:41:53 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,14 +1,20 @@
-# Load libraries
-library(data.table)
+## Getting full dataset
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
-# Load whole data into workspace using fread
-data <- fread('household_power_consumption.txt', header=T, sep=';')
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
 
-# Subset only Feb 1 and 2
-feb <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 
-# Plot histogram
-par(mfrow=c(1,1))
-png(filename='plot1.png', width=480, height=480, bg='transparent')
-hist(as.numeric(feb$Global_active_power), xlab='Global Active Power (kilowatts)', main='Global Active Power', col='red')
+## Plot 1
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
